489558f3405edbb79fedb14d75bb5dc983851101
Santiago Valencia <s.valencia@uniandes.edu.co>
2018-11-20 12:13:36 +0000

    grafica

--- test.py
+++ test.py
@@ -5,3 +5,9 @@ import matplotlib
 matplotlib.use('Agg')
 import matplotlib.pyplot as plt
 print("hola mundo")
+
+x = np.linspace(0, 10, 100)
+y = np.cos(x)*np.sin(x)
+
+plt.plot(x, y)
+plt.savefig("wol.pdf")
