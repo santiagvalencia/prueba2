b7cd454543e7bd5e511c25745b4d9201e64cee92
Santiago Valencia <s.valencia@uniandes.edu.co>
2018-11-20 12:23:49 +0000

    cpp

---
+++ ol.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <cmath>
+#include <cstdlib>
+#include <ctime>
+#include <fstream>
+#include <array>
+#include <string>
+
+using namespace std;
+
+int main(){
+   cout << "hi";
+    
+}
